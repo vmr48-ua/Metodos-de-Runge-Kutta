a7b5a4c2cd3fbb1dd7a59238cdd10d63d8c917e9
marcomas56 <marcomassempere@gmail.com>
2024-11-16 17:47:54 +0100

    ALGO

--- runge_kutta.py
+++ runge_kutta.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-
-
-
-def RKII(r0,t,drdt, p= 1/2) -> np.ndarray:
+def RKII(r0,t,drdt, param ,p= 1/2) -> np.ndarray:
     '''
     Función que implementa el método de Runge-Kuta 2 genérico
+    drdt = func(t,r,param)
+    r0 = r(t = t[0])
     '''
+
+    import numpy as np
+
     a1 = 1- p
     a2 = p
     
@@ -20,16 +22,21 @@ def RKII(r0,t,drdt, p= 1/2) -> np.ndarray:
     r[0,:] = r0
 
     for i in range(N-1):
-        k1 = dt*drdt(r[i])
-        k2 = dt*drdt(r[i] + c*k1)
+        k1 = dt*drdt(t[i],r[i],param)
+        k2 = dt*drdt(t[i] + c*dt,r[i] + c*k1,param)
         r[i+1] = r[i] + a1*k1 + a2*k2
     
     return r
 
-def RKIV(r0,t,drdt) -> np.ndarray:
+def RKIV(r0,t,drdt, param) -> np.ndarray:
     '''
     Función que implementa el método de Runge-Kuta 4
+    drdt = func(t,r)
+    r0 = r(t = t[0])
     '''
+
+    import numpy as np
+
     dt = t[1]-t[0]
     N = len(t)
 
@@ -38,10 +45,35 @@ def RKIV(r0,t,drdt) -> np.ndarray:
     r[0,:] = r0
 
     for i in range(N-1):
-        k1 = dt*drdt(r[i])
-        k2 = dt*drdt(r[i] + k1/2)
-        k3 = dt*drdt(r[i] + k2/2)
-        k4 = dt*drdt(r[i] + k3)
+        k1 = dt*drdt(t[i],r[i],param)
+        k2 = dt*drdt(t[i] + dt/2,r[i] + k1/2,param)
+        k3 = dt*drdt(t[i] + dt/2,r[i] + k2/2,param)
+        k4 = dt*drdt(t[i] + dt,r[i] + k3,param)
+        r[i+1] = r[i] + (k1+ 2*k2 + 2*k3 + k4)/6
+    
+    return r
+
+def RKIV(r0,t,drdt, param) -> np.ndarray:
+    '''
+    Función que implementa el método de Runge-Kuta 4
+    drdt = func(t,r)
+    r0 = r(t = t[0])
+    '''
+
+    import numpy as np
+
+    dt = t[1]-t[0]
+    N = len(t)
+
+    r = np.zeros((N,len(r0)))
+
+    r[0,:] = r0
+
+    for i in range(N-1):
+        k1 = dt*drdt(t[i],r[i],param)
+        k2 = dt*drdt(t[i] + dt/2,r[i] + k1/2,param)
+        k3 = dt*drdt(t[i] + dt/2,r[i] + k2/2,param)
+        k4 = dt*drdt(t[i] + dt,r[i] + k3,param)
         r[i+1] = r[i] + (k1+ 2*k2 + 2*k3 + k4)/6
     
     return r
