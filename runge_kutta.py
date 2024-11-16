1b0fb2146c86ede4c32d2e66d2ec0d83c17d9d1c
marcomas56 <marcomassempere@gmail.com>
2024-11-16 15:52:22 +0000

    algo

--- runge_kutta.py
+++ runge_kutta.py
@@ -1,5 +1,19 @@
+import numpy as np
 
 
-def RKIV():
+def RKIV(r0,drdt,dt,N) -> np.ndarray:
+    '''
+    Función que implementa el método de Runge-Kuta 4
+    '''
+    r = np.zeros((N,len(r0)))
 
-    return ...
+    r[0,:] = r0
+
+    for i in range(N-1):
+        k1 = dt*drdt(r[i])
+        k2 = dt*drdt(r[i] + k1/2)
+        k3 = dt*drdt(r[i] + k2/2)
+        k4 = dt*drdt(r[i] + k3)
+        r[i+1] = r[i] + (k1+ 2*k2 + 2*k3 + k4)/6
+    
+    return r
