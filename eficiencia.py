3ce21a978ebc555661a194f006f645ce277a37fd
vmr48-ua <vmr48@gcloud.ua.es>
2025-01-17 16:31:15 +0100

    eficiencia

---
+++ eficiencia.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+"""
+Vamos a calcular el error de la solución analítica de 
+la ecuación de Schrödinger frente a la solución numérica
+que proporcionan los distintos métodos de Runge-Kutta.
+"""
+
+def error():
+    return ...
+
+def timear(funcion, param):
+    """ 
+    Devuelve el tiempo que tarda en ejecutarse una función
+    """
+    t0 = time.perf_counter()
+    funcion(param)
+    t1 = time.perf_counter()
+    return(np.abs(t1-t0))
+
+def plot_eficiencia():
+    fig1,ax1 = plt.subplots()
+    ax1.set_title('Error de la parte real de $\\Psi (x)$\nfrente al tiempo de ejecución')
+    ax1.set_xlabel('Tiempo de ejecución')
+    ax1.set_ylabel('Error')
+    
+    fig2,ax2 = plt.subplots()
+    ax2.set_title('Error máximo de la parte real de $\\Psi (x)$\nfrente a tiempo de computación')
+    ax1.set_xlabel('Tiempo de computación')
+    ax1.set_ylabel('Error máximo')
+
+    return fig1, fig2
